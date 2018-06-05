ab6d70e854523e2fc98d092a35055405822662f3
Yuhan Zhou <yuhanzhou@yuhans-air.wireless.davidson.edu>
2018-06-05 14:55:56 -0400

    release 1 finished

---
+++ todo-react/src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo'
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    let currentTime = new Date(1997,10,24,1,1,1,1);
+    this.state = {
+      todos: [{description: 'default', dateDue: currentTime.toString(), isComplete: true, _id: 0, color: "black"},
+      {description: 'default2', dateDue: null, isComplete: false, _id: 1, color: "black"}],
+      input: "",
+      date: Date.now(),
+      counter: 2,
+      ordering: "default",
+    };
+  }
+
+  onChange = (event) => {
+    event.preventDefault();
+    this.setState({...this.state, input: event.target.value});
+  }
+
+  onAddToDo = event =>{
+    event.preventDefault();
+    //description = event.
+      let newToDo = {description: this.state.input, dateDue: null, isComplete: false, _id: this.state.counter, color: "black"};
+      let todos = [...this.state.todos];
+      todos.push(newToDo);
+      this.setState({...this.state, todos:todos, counter:this.state.counter+1});
+  }
+
+
+  onDeleteTodo = id => {
+    let todos = this.state.todos;
+    let i = todos.findIndex((todo) => todo._id===id);
+    todos.splice(i, 1);
+    this.setState({...this.state,todos});
+  }
+
+  deleteAll = () => {
+    let todos = [];
+    this.setState({...this.state,todos});
+  }
+
+  markAsUncomplete = id => {
+    //JSON.parse(JSON.stringify(array))
+    let todos = [...this.state.todos];
+    todos.find((todo)=>todo._id===id).isComplete = false;
+    // let newToDo = {description: this.state.todos[i].description,
+    //   dateDue: this.state.todos[i].dateDue, isComplete: false,
+    //   _id: this.state.todos[i]._id};
+    this.setState({...this.state, todos:todos});
+  }
+
+  markAsComplete = id => {
+    // console.log("here");
+    let todos = [...this.state.todos];
+    // console.log(id);
+    todos.find((todo)=>todo._id===id).isComplete = true;
+    this.setState({...this.state, todos:todos});
+  }
+
+  changeDate = event => {
+    event.preventDefault();
+    //console.log(event.target.value);
+    this.setState({...this.state, date: event.target.value});
+    //console.log(this.state.date);
+  }
+
+  setDate = (event, id) => {
+    event.preventDefault();
+    //console.log("here");
+    let todos = [...this.state.todos];
+    todos.find((todo)=>todo._id===id).dateDue = this.state.date;
+    this.setState([...this.state, todos:todos]);
+  }
+
+  setOrdering = () => {
+    console.log("here");
+    this.setState({...this.state, ordering: "alphabetically"});
+  }
+
+  setOrderingByDate = () => {
+    console.log("here");
+    this.setState({...this.state, ordering: "date"});
+  }
+
+  setColor = (event, id) => {
+    event.preventDefault();
+    let todos = [...this.state.todos];
+    todos.find((todo)=>todo._id===id).color = event.target.value;
+    this.setState({...this.state, todos:todos})
+  }
+
+  render() {
+    return (
+      <div>
+        <Todo todos={this.state.todos} onDeleteTodo={this.onDeleteTodo}
+        onAddToDo={this.onAddToDo} onChange={this.onChange} input={this.state.input}
+        markAsUncomplete={this.markAsUncomplete} markAsComplete={this.markAsComplete}
+      changeDate={this.changeDate} setDate={this.setDate}
+      ordering={this.state.ordering}
+        setOrdering={this.setOrdering} setOrderingByDate={this.setOrderingByDate}
+        deleteAll={this.deleteAll} setColor={this.setColor}
+        color={this.state.color}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ todo-react/src/components/Todo.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+
+// function Todo(props){
+//   return (<div> clssName="todo" {props.value.description}</div>);
+// }
+
+class Todos extends Component {
+  // renderTodo(i){
+  //   return (<Todo value={this.props.todos[i].description}/>);
+  // }
+  reorder(array){
+    let temp = [...array];
+    temp.sort((a, b) => a.description.localeCompare(b.description));
+    return temp;
+  }
+
+  reorderDate(array){
+    let temp = [...array];
+    temp.sort((a, b) => a.dateDue.localeCompare(b.dateDue));
+    return temp;
+  }
+
+  render(){
+    let completed = this.props.todos.filter(function (todo) {return todo.isComplete === true});
+    let uncompleted = this.props.todos.filter(function (todo) {return todo.isComplete === false});
+    if(this.props.ordering==="alphabetically"){
+      completed = this.reorder(completed);
+      uncompleted = this.reorder(uncompleted);
+    } else if(this.props.ordering==="date"){
+      completed = this.reorderDate(completed);
+      uncompleted = this.reorderDate(uncompleted);
+    }
+    // this.props.todos.map((todo) => {
+    //   if (todo.isComplete) completed.push(todo);
+    //   else uncompleted.push(todo);
+    // });
+
+    return (
+
+        <div className="todos-container">
+        <div className="Ordering options">
+          <button onClick={this.props.setOrdering}>alphabetically</button>
+          <button onClick={this.props.setOrderingByDate}>by date</button>
+        </div>
+        <div className="completed-container">
+        <h3>Completed Tasks</h3>
+        {completed.map((todo, i) => {
+          return(
+            <div key={todo._id}>
+              <div style={{color:todo.color}}>{todo.description}</div>
+              {todo.dateDue ? (<div>{todo.dateDue}</div>) : (
+                <div>
+                  <div>Due date not set</div>
+                    <input id="date" type="date"/>
+                </div>
+
+              )}
+              <button onClick={()=>this.props.onDeleteTodo(todo._id)}>Delete</button>
+              <button onClick={()=>this.props.markAsUncomplete(todo._id)}>Mark as uncompleted</button>
+              <select onChange={(event)=>this.props.setColor(event, todo._id)}>
+                <option value="black">black</option>
+                <option value="red">red</option>
+                <option value="yellow">yellow</option>
+                <option value="blue">blue</option>
+                <option value="pink">pink</option>
+                <option value="aqua">aqua</option>
+                <option value="indigo">indigo</option>
+              </select>
+            </div>);
+        })
+      }
+      </div>
+      <div className="uncompleted-container">
+      <h3>Uncompleted Tasks</h3>
+          {
+            uncompleted.map((todo, i) => {
+            return(
+              <div key={todo._id}>
+                <div style={{color:todo.color}}>{todo.description}</div>
+                {todo.dateDue ? (<div>{todo.dateDue}</div>) : (
+                  <div>
+                    <div>Due date not set</div>
+                      <input id="date" type="datetime-local" onChange={this.props.changeDate}/>
+                      <button onClick={(event)=>this.props.setDate(event, todo._id)}>Set Due Date</button>
+                  </div>
+                )}
+                <button onClick={()=>this.props.onDeleteTodo(todo._id)}>Delete</button>
+                <button onClick={()=>this.props.markAsComplete(todo._id)}>Mark as completed</button>
+                <select onChange={(event)=>this.props.setColor(event, todo._id)}>
+                  <option value="black">black</option>
+                  <option value="red">red</option>
+                  <option value="yellow">yellow</option>
+                  <option value="blue">blue</option>
+                  <option value="pink">pink</option>
+                  <option value="aqua">aqua</option>
+                  <option value="indigo">indigo</option>
+                </select>
+              </div>);
+          })
+        }
+      </div>
+      <div>
+        <form onSubmit={this.props.onAddToDo}>
+          Add a todo: <input type="text" name="description" onChange={this.props.onChange}/>
+          <input type="submit" value="submit"/>
+        </form>
+      </div>
+      <div>
+          <button onClick={this.props.deleteAll}>Delete all todos</button>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Todos;
